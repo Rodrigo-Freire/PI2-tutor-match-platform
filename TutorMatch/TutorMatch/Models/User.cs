5d3764a4ada590880d4d9fc6f7958826d30fad47
Rodrigo <rodrigo.freire@msn.com>
2024-10-19 14:48:43 -0300

    [Feature] Classe User criada com propriedades personalizadas para o registro

---
+++ TutorMatch/TutorMatch/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TutorMatch.Models
+	{
+	public class User : IdentityUser
+		{
+		// Propriedade correspondente ao campo "Nome" no formulário de cadastro
+		public string Nome { get; set; }
+
+		// Propriedade correspondente ao campo "Tipo de Usuário"
+		public string UserType { get; set; }
+
+		// Você pode adicionar outras propriedades conforme necessário
+		}
+	}
