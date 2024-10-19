bbf24c5d8492ddc214daf25b0ebc8ad357c8b118
Rodrigo <rodrigo.freire@msn.com>
2024-10-19 14:52:42 -0300

    [Feature] Configuração do Identity e autenticação no Program.cs

--- TutorMatch/TutorMatch/Program.cs
+++ TutorMatch/TutorMatch/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using TutorMatch.Data;
+using TutorMatch.Models; // Adicionar o namespace para a classe User
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +11,34 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-	.AddEntityFrameworkStores<ApplicationDbContext>();
+// Configurar o Identity para usar a classe User
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+	// Configurações de senha
+	options.Password.RequireDigit = true;
+	options.Password.RequiredLength = 6;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireLowercase = true;
+
+	// Configurações de bloqueio
+	options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+	options.Lockout.MaxFailedAccessAttempts = 5;
+	options.Lockout.AllowedForNewUsers = true;
+
+	// Configurações de usuário
+	options.User.RequireUniqueEmail = true;
+})
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddDefaultTokenProviders();
+
+// Configurar autenticação via cookie
+builder.Services.ConfigureApplicationCookie(options =>
+{
+	options.LoginPath = "/Account/Login";
+	options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -33,6 +60,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Adicionar autenticação e autorização
+app.UseAuthentication(); // Adiciona essa linha
 app.UseAuthorization();
 
 app.MapControllerRoute(
