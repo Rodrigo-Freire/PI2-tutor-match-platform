599aaa0a79e2ba98517fcabb75d33fa7a260cf11
Rodrigo <rodrigo.freire@msn.com>
2024-10-19 16:09:23 -0300

    [Feature] Adicionando required nas variáveis

--- TutorMatch/TutorMatch/Models/User.cs
+++ TutorMatch/TutorMatch/Models/User.cs
@@ -5,10 +5,10 @@ namespace TutorMatch.Models
 	public class User : IdentityUser
 		{
 		// Propriedade correspondente ao campo "Nome" no formulário de cadastro
-		public string Name { get; set; }
+		public required string Name { get; set; }
 
 		// Propriedade correspondente ao campo "Tipo de Usuário"
-		public string UserType { get; set; }
+		public required string UserType { get; set; }
 
 		// Você pode adicionar outras propriedades conforme necessário
 		}
