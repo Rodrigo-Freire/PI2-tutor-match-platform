8339e2e9a4545f0f42b865672687b8e4c7cd7b5e
Rodrigo <rodrigo.freire@msn.com>
2024-10-19 09:45:03 -0300

    [Model] Adição do RegisterViewModel - Criado o modelo RegisterViewModel com propriedades Name, Email, Password, ConfirmPassword e UserType. - Adicionadas validações usando Data Annotations.

---
+++ TutorMatch/TutorMatch/Models/RegisterViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TutorMatch.Models
+	{
+	public class RegisterViewModel
+		{
+		[Required(ErrorMessage = "O nome é obrigatório.")]
+		public required string Name { get; set; }
+
+		[Required(ErrorMessage = "O e-mail é obrigatório.")]
+		[EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
+		public required string Email { get; set; }
+
+		[Required(ErrorMessage = "A senha é obrigatória.")]
+		[DataType(DataType.Password)]
+		[StringLength(100, ErrorMessage = "A {0} deve ter pelo menos {2} caracteres.", MinimumLength = 6)]
+		public required string Password { get; set; }
+
+		[DataType(DataType.Password)]
+		[Display(Name = "Confirme sua senha")]
+		[Compare("Password", ErrorMessage = "As senhas não conferem.")]
+		public required string ConfirmPassword { get; set; }
+
+		[Required(ErrorMessage = "O tipo de usuário é obrigatório.")]
+		public required string UserType { get; set; }  // Diferenciar entre aluno e professor
+		}
+	}
